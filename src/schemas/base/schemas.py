70e829774a2a236f6b61c84c8d294db40ec0632e
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2025-08-19 13:57:13 -0300

    Add base schemas

---
+++ src/schemas/base/schemas.py
@@ -0,0 +1,13 @@
+import uuid
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class UUIDSchema(BaseModel):
+    id: uuid.UUID
+
+
+class TimestampSchema(BaseModel):
+    created_at: datetime
+    updated_at: datetime
